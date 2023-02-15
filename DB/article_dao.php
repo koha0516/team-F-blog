0300cab67e62c4aafe9f0e7429165f8a0208fc02
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2023-02-15 11:24:36 +0900

    投稿機能（途中）

--- DB/article_dao.php
+++ DB/article_dao.php
@@ -1,18 +1,18 @@
 <?php
 require_once "get_connect.php";
 
-//ユーザ情報をINSERTするメソッド
-function article_register($name, $birth, $mail, $salt, $password) {
+//投稿内容をINSERTするメソッド
+function article_register($title, $contents, $tag, $user, $published) {
   try {
-    $sql = "INSERT INTO users (user_name, user_birth, user_mail, salt, password, delete_frag) VALUES (:name, :birth, :mail, :salt, :password, 1)";
+    $sql = "INSERT INTO articles (title, article_content, tag_id, user_id, published, delete_frag) VALUES (:title, :contents, :tag, :user, :published, 1)";
     $stm = get_connect()->prepare($sql);
 
     // プレースホルダに値をバインドする
-    $stm->bindValue(':name', $name, PDO::PARAM_STR);
-    $stm->bindValue(':birth', $birth, PDO::PARAM_STR);
-    $stm->bindValue(':mail', $mail, PDO::PARAM_STR);
-    $stm->bindValue(':salt', $salt, PDO::PARAM_STR);
-    $stm->bindValue(':password', $password, PDO::PARAM_STR);
+    $stm->bindValue(':title', $title, PDO::PARAM_STR);
+    $stm->bindValue(':contents', $contents, PDO::PARAM_STR);
+    $stm->bindValue(':tag', $tag, PDO::PARAM_STR);
+    $stm->bindValue(':user', $user, PDO::PARAM_STR);
+    $stm->bindValue(':published', $published, PDO::PARAM_STR);
 
     // SQL文を実行する
     $stm->execute();
@@ -27,43 +27,11 @@ function article_register($name, $birth, $mail, $salt, $password) {
   }
 }
 
-// nameとpasswordを元にユーザ情報を取ってくる関数
-function login($name, $password) {
-  try {
-    // sql文の構築
-    $sql = "SELECT user_id, user_name, user_birth, user_mail FROM users WHERE user_name = :mail AND password = :password AND delete_frag > 0";
-    $stm = get_connect()->prepare($sql);
-    // プレースホルダに値をバインドする
-    $stm->bindValue(":mail", $name, PDO::PARAM_STR);
-    $stm->bindValue(":password", $password, PDO::PARAM_STR);
-    // sql文の実行
-    $stm->execute();
-
-    return $stm->fetch(PDO::FETCH_ASSOC);
-
-  } catch (PDOException $e) {
-    // エラー発生
-    echo $e->getMessage();
-  } finally {
-    // DB接続を閉じる
-    $pdo = null;
-  }
-}
-
-function get_articles() {
-  try {
-    // sql文の構築
-    $sql = "SELECT * FROM articles WHERE delete_frag < 1";
-    $stm = get_connect()->prepare($sql);
-    $stm->execute();
-    // 検索結果を配列として全件取得する
-    return $stm->fetchAll(PDO::FETCH_ASSOC);
-
-  } catch (PDOException $e) {
-    // エラー発生
-    echo $e->getMessage();
-  } finally {
-    // DB接続を閉じる
-    $pdo = null;
+    //    文字数チェックをする関数
+class user_function {
+  static function length_validation($str, $max, $min) {
+  //    文字数カウント
+  $str = mb_strlen($str, "UTF-8");
+  return $str <= $max && $str >= $min;
   }
 }
