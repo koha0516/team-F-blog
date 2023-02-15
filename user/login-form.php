a244d1630e9a8bb6753fb1f880297aaaa2b58a1d
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2023-02-15 16:53:51 +0900

    ログインページのヘッダー追加

--- user/login-form.php
+++ user/login-form.php
@@ -21,7 +21,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
   <title>login</title>
 </head>
 
@@ -36,14 +36,35 @@
     </nav>
   </header>
 
-  <div id="login-form">
+  <!--コンテンツ-->
+  <div class="box">
+    <h2>ログイン画面</h2>
     <form action="login.php" method="post">
-      <input type="text" name="name" placeholder="氏名" value="<?php echo $name; ?>" required><br>
-      <input type="password" name="pass" placeholder="パスワード" required><br>
 
-      <a href="../">戻る</a>
-      <input type="submit" value="ログイン">
+      <div class="cp_iptxt">
+        <label class="ef">
+          <input type="text" name="name" placeholder="お名前" value="<?php echo $name; ?>" required>
+        </label>
+      </div>
+
+      <div class="cp_iptxt">
+        <label class="ef">
+          <input type="password" name="pw" placeholder="パスワード" required>
+        </label>
+      </div>
+
+      <div style="display:inline-flex;">
+        <button type="submit" value="ログイン" class="button">ログイン</button>
     </form>
+
+    <div class="btn2"><a href="index.php">戻る</a></div><br>
+  </div>
+
+  <div style="margin-top: 10px;">
+    <a href="signUp-form.php">新規登録</a>
+    <a href="edit-password.php">パスワードを忘れた方はこちら</a>
+  </div>
+
   </div>
 
 </body>
--- user/signUp-form.php
+++ user/signUp-form.php
@@ -39,7 +39,7 @@ if (isset($_SESSION['error_pass'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
   <title>signUp</title>
 </head>
 <body>
