189f9e0a2deb6d6414b7b959f1058d41515c05f8
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2023-03-22 10:04:41 +0900

    [Add]フォローリストデザイン反映

--- user/follow-list.php
+++ user/follow-list.php
@@ -21,7 +21,7 @@ if ($_SESSION['user_info'] !== null) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/follow_style.css">
   <title>ミジンコ|フォローリスト</title>
 </head>
 
